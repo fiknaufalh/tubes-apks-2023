15bc4b47eb25bf6608bda218035805ddbe3bdebc
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-11-17 20:16:39 +0700

    feat: fix bug

--- src/index.js
+++ src/index.js
@@ -10,18 +10,14 @@ const prometheus = require("prom-client");
 const register = new prometheus.Registry();
 prometheus.collectDefaultMetrics({register});
 
-// Create a custom histogram metric
-const httpRequestTimer = new prometheus.Histogram({
-	name: 'http_request_duration_seconds',
-	help: 'Duration of HTTP requests in seconds',
-	labelNames: ['method', 'route', 'code'],
-	buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
-  });
-  
-// Register the histogram
-register.registerMetric(httpRequestTimer);
-
 /** Debugging */
+const httpRequestDurationMicroseconds = new prometheus.Histogram({
+	name: 'http_request_duration_seconds',
+	help: 'Duration of HTTP requests in microseconds',
+	labelNames: ['method', 'route', 'code'],
+	buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+});
+register.registerMetric(httpRequestDurationMicroseconds)
 // Mock slow endpoint, waiting between 3 and 6 seconds to return a response
 const createDelayHandler = async (req, res) => {
 	if ((Math.floor(Math.random() * 100)) === 0) {
@@ -31,7 +27,7 @@ const createDelayHandler = async (req, res) => {
 	const delaySeconds = Math.floor(Math.random() * (6 - 3)) + 3
 	await new Promise(res => setTimeout(res, delaySeconds * 1000))
 	res.end('Slow url accessed!');
-  };
+};
 
 const start = async () => {
 	try {
@@ -81,14 +77,6 @@ const start = async () => {
 		app.use(showtimeRouter);
 		app.use(reservationRouter);
 		app.use(invitationsRouter);
-		app.use((req, res, next) => {
-			// Histogram
-			httpRequestTimer.observe()
-
-			// Counter
-			
-			next()
-		})
 
 		app.get("/health", (req, res) => {
 			res.send({ "API Server": "OK" });
@@ -102,7 +90,7 @@ const start = async () => {
 
 		/* Debugging for slow request*/
 		app.get('/slow', async (req, res) => {
-			const end = httpRequestTimer.startTimer();
+			const end = httpRequestDurationMicroseconds.startTimer();
 			const route = req.route.path;
 			await createDelayHandler(req, res);
 			end({ route, code: res.statusCode, method: req.method });
@@ -115,6 +103,7 @@ const start = async () => {
 		});
 
 		app.listen(port, () => console.log(`app is running in PORT: ${port}, metrics can be accessed using '/metrics' endpoint`));
+
 	} catch (err) {
 		console.log(err);
 		app.get("/health", (req, res) => {
